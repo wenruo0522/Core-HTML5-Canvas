4225bf83fb9ebdc63e3da8d08b949115a599f4cf
wenruo0522 <376567610@qq.com>
2021-05-11 00:53:44 +0800

    add: example-2.1 && example-2.2 && example-2.3

---
+++ ch02/example-2.1/example.js
@@ -0,0 +1,17 @@
+
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+
+context.lineJoin = 'round'
+context.lineWidth = 30
+
+context.font = '24px Helvetica'
+context.fillText('Click anywhere to erase', 175, 40)
+
+context.strokeRect(75, 100, 200, 200)
+context.fillRect(325, 100, 200, 200)
+
+context.canvas.onmousedown = function(e) {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
